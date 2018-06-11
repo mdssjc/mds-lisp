bf16a14e15ef8cf35b59ad7ec31e7d9dc9e2b496
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-11 18:56:16 -0300

    3 funções para o random posicionamento; Refatoração para funções.

--- clojure/the-nature-of-code/book/src/book/chapter01/walker.clj
+++ clojure/the-nature-of-code/book/src/book/chapter01/walker.clj
@@ -7,6 +7,23 @@
 
 (defrecord Walker [x y])
 
+(defn rnd-4 [x y]
+  (let [choice (int (q/random 4))]
+    (cond (== choice 0) [(inc x) y]
+          (== choice 1) [(dec x) y]
+          (== choice 2) [x (inc y)]
+          :else         [x (dec y)])))
+
+(defn rnd-8a [x y]
+  (let [stepx (+ x (- (int (q/random 3)) 1))
+        stepy (+ y (- (int (q/random 3)) 1))]
+    [stepx stepy]))
+
+(defn rnd-8b [x y]
+  (let [stepx (+ x (q/random -1 1))
+        stepy (+ y (q/random -1 1))]
+    [stepx stepy]))
+
 (defn display [w]
   (q/stroke 0)
   (q/point (-> @w :x) (-> @w :y)))
@@ -14,11 +31,7 @@
 (defn step [w]
   (let [wx (-> @w :x)
         wy (-> @w :y)
-        choice (int (q/random 4))
-        [x, y] (cond (== choice 0) [(inc wx), wy]
-                     (== choice 1) [(dec wx), wy]
-                     (== choice 2) [wx, (inc wy)]
-                     :else         [wx, (dec wy)])]
+        [x y] (rnd-8b wx wy)]
     (swap! w assoc :x x)
     (swap! w assoc :y y)))
 
