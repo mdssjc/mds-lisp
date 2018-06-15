e89aeaa1a4106d4bd9a0f69ecd961f97361c2847
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-14 21:17:23 -0300

    Example I.4: Gaussian distribution.

---
+++ clojure/the-nature-of-code/book/src/book/chapter01/exampleI4.clj
@@ -0,0 +1,26 @@
+(ns book.chapter01.exampleI4
+  (:require [quil.core :as q]))
+
+;; Example I.4: Gaussian distribution
+
+(def WIDTH  640)
+(def HEIGHT 360)
+
+(defn setup []
+  (q/background 255))
+
+(defn draw []
+  (q/no-stroke)
+  (q/fill 175 10)
+
+  (let [num  (q/random-gaussian)
+        sd   60
+        mean 320
+        x    (+ (* sd num) mean)]
+
+    (q/ellipse x 180 16 16)))
+
+(q/defsketch run
+  :size  [WIDTH HEIGHT]
+  :setup setup
+  :draw  draw)
