b9a09c228e54e6de1313f35b7eec61279717174e
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-18 21:42:42 -0300

    Exercise I.9.

---
+++ clojure/the-nature-of-code/book/src/book/chapter01/exerciseI9.clj
@@ -0,0 +1,31 @@
+(ns book.chapter01.exerciseI9
+  (:require [quil.core :as q]))
+
+;; Exercise I.9
+
+(def WIDTH  640)
+(def HEIGHT 360)
+(def increment 0.01)
+(def z-increment 0.01)
+(def z (atom 0))
+
+(defn setup []
+  ;; ---
+  )
+
+(defn draw []
+  (q/background 0)
+  (q/noise-detail 8 0.65)
+  (q/pixels)
+  (doseq [x (range WIDTH)
+          y (range HEIGHT)]
+    (let [bright (q/map-range (q/noise (* x increment) (* y increment) @z) 0 1 0 255)]
+      (aset-int (q/pixels) (+ x (* y WIDTH)) (q/color bright))))
+  (q/update-pixels)
+  (swap! z (fn [_] (+ @z z-increment))))
+
+(q/defsketch run
+  :size  [WIDTH HEIGHT]
+  :setup setup
+  :draw  draw
+  :renderer :p2d)
