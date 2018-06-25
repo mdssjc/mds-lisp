ac853d2305e720c7e23c545710119f15b5f2d476
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-24 23:06:09 -0300

    Corrigido o problema.

--- clojure/the-nature-of-code/book/src/book/introduction/exampleI6.clj
+++ clojure/the-nature-of-code/book/src/book/introduction/exampleI6.clj
@@ -2,7 +2,6 @@
   (:require [quil.core :as q]))
 
 ;; Example I.6: 2D Perlin noise
-;; FIXME: problemas na renderização do sketch
 
 (def WIDTH  640)
 (def HEIGHT 360)
@@ -13,15 +12,12 @@
 
 (defn draw []
   (q/background 255)
-  (q/pixels)
   (doseq [x (range WIDTH)
-          y (range HEIGHT)]
-    (let [bright (q/map-range (q/noise (* x increment) (* y increment)) 0 1 0 255)]
-      (aset-int (q/pixels) (+ x (* y WIDTH)) (q/color bright))))
-  (q/update-pixels))
+          y (range HEIGHT)
+          :let [bright (* (q/noise (* x increment) (* y increment)) 255)]]
+    (q/set-pixel x y (q/color bright))))
 
 (q/defsketch run
   :size  [WIDTH HEIGHT]
   :setup setup
-  :draw  draw
-  :renderer :p2d)
+  :draw  draw)
