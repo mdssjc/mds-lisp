efe9b97771acc1d351c9829099511a08a3cb4f34
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-26 22:05:16 -0300

    PVector adicionado.

---
+++ clojure/the-nature-of-code/book/src/book/chapter1/PVector.clj
@@ -0,0 +1,20 @@
+(ns book.chapter1.PVector
+  (:use clojure.test))
+
+(defn make-pvector [x y]
+  {:x x
+   :y y})
+
+(defn add [v1 v2]
+  {:x (+ (:x v1) (:x v2))
+   :y (+ (:y v1) (:y v2))})
+
+
+
+;; Unit Testing
+
+(def a (make-pvector 5 2))
+(def b (make-pvector 3 4))
+
+(deftest pvector-tests
+  (is {:x 8 :y 6} (add a b)))
