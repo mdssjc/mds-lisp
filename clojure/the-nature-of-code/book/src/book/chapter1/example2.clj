4e478c0ffff52f2b20033788a0d50be4d12a4f3f
Marcelo dos Santos <mdssjc@gmail.com>
2018-06-27 21:34:14 -0300

    Example 1.2: Bouncing ball with PVectors!

---
+++ clojure/the-nature-of-code/book/src/book/chapter1/example2.clj
@@ -0,0 +1,42 @@
+(ns book.chapter1.example2
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [book.chapter1.PVector :as v]))
+
+;; Example 1.2: Bouncing ball with PVectors!
+
+(defn setup []
+  {:location (v/make-pvector 100 100)
+   :velocity (v/make-pvector 2.5 5)})
+
+(defn draw [state]
+  (q/background 255)
+
+  (q/stroke 0)
+  (q/fill 175)
+  (q/ellipse (get-in state [:location :x])
+             (get-in state [:location :y])
+             16 16))
+
+(defn check-bouncing [location velocity dimension]
+  (if (or (> location (dimension))
+          (< location 0))
+    (* velocity -1)
+    velocity))
+
+(defn update-state [state]
+  (let [location (v/add (:location state) (:velocity state))]
+    {:location location
+     :velocity (v/make-pvector (check-bouncing (:x location)
+                                               (get-in state [:velocity :x])
+                                               q/width)
+                               (check-bouncing (:y location)
+                                               (get-in state [:velocity :y])
+                                               q/height))}))
+
+(q/defsketch run
+  :size   [640 360]
+  :setup  setup
+  :draw   draw
+  :update update-state
+  :middleware [m/fun-mode])
