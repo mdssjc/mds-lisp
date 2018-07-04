b2369b39f1301c05a05f8dc685d6325394eead8d
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-03 22:15:19 -0300

    Exercise 387.

--- racket/HtDP2e/IV/23-Simultaneous Processing.rkt
+++ racket/HtDP2e/IV/23-Simultaneous Processing.rkt
@@ -4,3 +4,47 @@
 ;; 23-Simultaneous Processing.rkt
 ;; IV - Intertwined Data
 ;; 23 - Simultaneous Processing
+
+
+
+;; 23.1 Processing Two Lists Simultaneously: Case 1
+
+
+;; =================
+;; Functions:
+
+; [List-of Number] [List-of Number] -> [List-of Number]
+; replaces the final '() in front with end
+(check-expect (replace-eol-with '() '(a b))
+              '(a b))
+(check-expect (replace-eol-with (cons 1 '()) '(a))
+              (cons 1 '(a)))
+(check-expect (replace-eol-with (cons 2 (cons 1 '())) '(a))
+              (cons 2 (cons 1 '(a))))
+
+(define (replace-eol-with front end)
+  (cond [(empty? front) end]
+        [else
+         (cons (first front)
+               (replace-eol-with (rest front) end))]))
+
+;; Exercise 387
+
+; [List-of Symbol] [List-of Number] -> [List-of (list Symbol Number)]
+; produces all possible ordered pairs of symbols and numbers
+(check-expect (cross '(a b c) '(1 2))
+              '((a 1) (a 2) (b 1) (b 2) (c 1) (c 2)))
+(check-expect (cross '() '(1 2))   '())
+(check-expect (cross '(a b c) '()) '())
+
+(define (cross los lon)
+  (local (; Symbol [List-of Number] -> [List-of (list Symbol Number)
+          (define (cross-one s lon)
+            (cond [(empty? lon) '()]
+                  [else
+                   (cons (list s      (first lon))
+                         (cross-one s (rest  lon)))])))
+    (cond [(empty? los) '()]
+          [else
+           (append (cross-one (first los) lon)
+                   (cross     (rest  los) lon))])))
