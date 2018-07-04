0a1dccf375e61825f5493e8cda2aa9c09a968038
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-03 21:33:27 -0300

    Placeholder para o 23 - Simultaneous Processing.

---
+++ racket/HtDP2e/IV/23-Simultaneous Processing.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |23-Simultaneous Processing|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; 23-Simultaneous Processing.rkt
+;; IV - Intertwined Data
+;; 23 - Simultaneous Processing
