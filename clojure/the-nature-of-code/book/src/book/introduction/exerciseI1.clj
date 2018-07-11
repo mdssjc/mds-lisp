8ffb3b707b5bb9035acdd8dc695c662e8476939c
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-10 22:07:02 -0300

    Refatoração com o middleware do quil.

--- clojure/the-nature-of-code/book/src/book/introduction/exerciseI1.clj
+++ clojure/the-nature-of-code/book/src/book/introduction/exerciseI1.clj
@@ -1,15 +1,26 @@
 (ns book.introduction.exerciseI1
   (:require [quil.core :as q]
-            [book.introduction.walker :as walker]))
+            [quil.middleware :as m]
+            [book.introduction.walker :as w]))
 
 ;; Exercise I.1
 
-(defn rnd-dr [x y]
-  (let [stepx (+ x (q/random -1 2))
-        stepy (+ y (q/random -1 2))]
-    [stepx stepy]))
+(defn setup []
+  (w/setup))
+
+(defn draw [state]
+  (w/display state))
+
+(defn update-state [state]
+  (w/step state
+          (fn [x y]
+            (let [stepx (+ x (q/random -1 2))
+                  stepy (+ y (q/random -1 2))]
+              [stepx stepy]))))
 
 (q/defsketch run
-  :size  walker/SIZE
-  :setup walker/setup
-  :draw  #(walker/draw rnd-dr))
+  :size   [640 360]
+  :setup  setup
+  :draw   draw
+  :update update-state
+  :middleware [m/fun-mode])
--- clojure/the-nature-of-code/book/src/book/introduction/walker.clj
+++ clojure/the-nature-of-code/book/src/book/introduction/walker.clj
@@ -1,11 +1,28 @@
 (ns book.introduction.walker
   (:require [quil.core :as q]))
 
-(def WIDTH  640)
-(def HEIGHT 360)
-(def SIZE [WIDTH HEIGHT])
+(defn make-walker []
+  {:x (/ (q/width)  2.0)
+   :y (/ (q/height) 2.0)})
 
-(defrecord Walker [x y])
+(defn setup []
+  (q/background 255)
+  (make-walker))
+
+(defn step [walker f]
+  (let [[x y] (f (:x walker)
+                 (:y walker))
+        cx    (q/constrain x 0 (- (q/width)  1))
+        cy    (q/constrain y 0 (- (q/height) 1))]
+    {:x cx
+     :y cy}))
+
+(defn display [walker]
+  (q/stroke 0)
+  (q/point (:x walker)
+           (:y walker)))
+
+;; Utils
 
 (defn rnd-8a [x y]
   (let [stepx (+ x (- (int (q/random 3)) 1))
@@ -16,38 +33,3 @@
   (let [stepx (+ x (q/random -1 1))
         stepy (+ y (q/random -1 1))]
     [stepx stepy]))
-
-(defn display [w]
-  (q/stroke 0)
-  (q/point (-> @w :x)
-           (-> @w :y)))
-
-(defn step [w func]
-  (let [[x y] (func (-> @w :x)
-                    (-> @w :y))
-        cx    (q/constrain x 0 (- WIDTH 1))
-        cy    (q/constrain y 0 (- HEIGHT 1))]
-    (swap! w assoc :x cx :y cy)))
-
-(def w (atom (Walker. 0 0)))
-
-(defn setup []
-  (swap! w assoc :x (/ WIDTH 2.0) :y (/ HEIGHT 2.0))
-  (q/background 255))
-
-(defn draw
-  ([]
-   (step    w rnd-8b)
-   (display w))
-  ([func]
-   (step    w func)
-   (display w))
-  ([func-step func-draw]
-   (step w func-step)
-   (func-draw (-> @w :x)
-              (-> @w :y))))
-
-(q/defsketch run
-  :size  SIZE
-  :setup setup
-  :draw  draw)
