0b65d5c4bdadf5746bd4e72146a31ec7435908c7
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-11 21:44:03 -0300

    Atualização do Clojure.

--- clojure/the-nature-of-code/book/project.clj
+++ clojure/the-nature-of-code/book/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mdssjc/mds-lisp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.10.0-alpha6"]
                  [quil "2.7.1"]
                  [cider/orchard "0.4.0-SNAPSHOT"]]
   :plugins [[refactor-nrepl    "2.4.0-SNAPSHOT"]
