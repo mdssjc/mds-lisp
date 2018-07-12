a0e64507ac51223df2b16b1b64e24de4b33a0ec3
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-12 20:55:39 -0300

    Adicionado a aceleração.

--- clojure/the-nature-of-code/course/src/course/session1/walker.clj
+++ clojure/the-nature-of-code/course/src/course/session1/walker.clj
@@ -3,12 +3,18 @@
   (:require [quil.core :as q]))
 
 (defn make-walker []
-  (new PVector (/ (q/width) 2) (/ (q/height) 2)))
+  {:pos (new PVector (/ (q/width) 2) (/ (q/height) 2))
+   :vel (new PVector 0 0)})
 
 (defn update [w]
-  (let [vel (new PVector (q/random -5 5) (q/random -5 5))]
-    (.add w vel)))
+  (let [mouse (new PVector (q/mouse-x) (q/mouse-y))
+        acc   (.setMag (.sub mouse (:pos w)) 0.4)
+        vel   (.add (:vel w) acc)
+        pos   (.add (:pos w) vel)]
+    {:pos pos
+     :vel vel}))
 
 (defn display [w]
   (q/fill 255)
-  (q/ellipse (.x w) (.y w) 48 48))
+  (let [pos (:pos w)]
+    (q/ellipse (.x pos) (.y pos) 48 48)))
