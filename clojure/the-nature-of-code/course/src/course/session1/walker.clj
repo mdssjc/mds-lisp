433872296243302e59aac7a47a569a5d00058752
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-13 21:55:18 -0300

    Incremento do vetor de aceleração.

--- clojure/the-nature-of-code/course/src/course/session1/walker.clj
+++ clojure/the-nature-of-code/course/src/course/session1/walker.clj
@@ -3,24 +3,25 @@
   (:require [quil.core :as q]))
 
 (defn make-walker []
-  {:pos  (new PVector (/ (q/width) 2) (/ (q/height) 2))
-   :vel  (new PVector 0 0)
-   :xoff  0.0
-   :yoff 10.0})
+  {:position     (new PVector (/ (q/width) 2) (/ (q/height) 2))
+   :velocity     (new PVector 0 0)
+   :acceleration (new PVector 0 0)
+   :xoff         0.0
+   :yoff         10.0})
 
 (defn update [w]
   (let [x   (* (q/noise (:xoff w)) (q/width))
         y   (* (q/noise (:yoff w)) (q/height))
-        xy  (new PVector x y)
-        acc (.setMag (.sub xy (:pos w)) 0.4)
-        vel (.add (:vel w) acc)
-        pos (.add (:pos w) vel)]
-    {:pos  pos
-     :vel  vel
-     :xoff (+ (:xoff w) 0.05)
-     :yoff (+ (:yoff w) 0.05)}))
+        acc (.setMag (.sub (new PVector x y) (:position w)) 0.4)
+        vel (.add (:velocity w) acc)
+        pos (.add (:position w) vel)]
+    {:position     pos
+     :velocity     vel
+     :acceleration acc
+     :xoff         (+ (:xoff w) 0.05)
+     :yoff         (+ (:yoff w) 0.05)}))
 
 (defn display [w]
   (q/fill 255)
-  (let [pos (:pos w)]
+  (let [pos (:position w)]
     (q/ellipse (.x pos) (.y pos) 48 48)))
