90ef44cd45e43cdbb46c02ff7bdba5b27bd819d9
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-13 21:16:14 -0300

    Implementado com Perlin noise.

--- clojure/the-nature-of-code/course/src/course/session1/walker.clj
+++ clojure/the-nature-of-code/course/src/course/session1/walker.clj
@@ -3,16 +3,22 @@
   (:require [quil.core :as q]))
 
 (defn make-walker []
-  {:pos (new PVector (/ (q/width) 2) (/ (q/height) 2))
-   :vel (new PVector 0 0)})
+  {:pos  (new PVector (/ (q/width) 2) (/ (q/height) 2))
+   :vel  (new PVector 0 0)
+   :xoff  0.0
+   :yoff 10.0})
 
 (defn update [w]
-  (let [mouse (new PVector (q/mouse-x) (q/mouse-y))
-        acc   (.setMag (.sub mouse (:pos w)) 0.4)
-        vel   (.add (:vel w) acc)
-        pos   (.add (:pos w) vel)]
-    {:pos pos
-     :vel vel}))
+  (let [x   (* (q/noise (:xoff w)) (q/width))
+        y   (* (q/noise (:yoff w)) (q/height))
+        xy  (new PVector x y)
+        acc (.setMag (.sub xy (:pos w)) 0.4)
+        vel (.add (:vel w) acc)
+        pos (.add (:pos w) vel)]
+    {:pos  pos
+     :vel  vel
+     :xoff (+ (:xoff w) 0.05)
+     :yoff (+ (:yoff w) 0.05)}))
 
 (defn display [w]
   (q/fill 255)
