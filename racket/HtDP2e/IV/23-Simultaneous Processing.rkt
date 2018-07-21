a20e69441f0ee71daa11939bfdbd185bdc8966c0
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-20 22:18:12 -0300

    Exercise 403.

--- racket/HtDP2e/IV/23-Simultaneous Processing.rkt
+++ racket/HtDP2e/IV/23-Simultaneous Processing.rkt
@@ -817,3 +817,73 @@
 
 ;; The hint results in the final process of optimizing the processing of two
 ;; lists.
+
+
+
+;; 23.7 Project: Database
+
+
+;; =================
+;; Data definitions:
+
+(define-struct db [schema content])
+; A DB is a structure:
+;   (make-db Schema Content)
+
+; A Schema is a [List-of Spec]
+; A Spec is a [List Label Predicate]
+; A Label is a String
+; A Predicate is a [Any -> Boolean]
+
+; A (piece of) Content is a [List-of Row]
+; A Row is a [List-of Cell]
+; A Cell is Any
+; constraint cells do not contain functions
+
+; integrity constraint In (make-db sch con),
+; for every row in con,
+; (I1) its length is the same as sch's, and
+; (I2) its ith Cell satisfies the ith Predicate in sch
+
+(define school-schema
+  `(("Name"    ,string?)
+    ("Age"     ,integer?)
+    ("Present" ,boolean?)))
+
+(define school-content
+  `(("Alice" 35 #true)
+    ("Bob"   25 #false)
+    ("Carol" 30 #true)
+    ("Dave"  32 #false)))
+
+(define school-db
+  (make-db school-schema
+           school-content))
+
+(define presence-schema
+  `(("Present"     ,boolean?)
+    ("Description" ,string?)))
+
+(define presence-content
+  `((#true  "presence")
+    (#false "absence")))
+
+(define presence-db
+  (make-db presence-schema
+           presence-content))
+
+;; Exercise 403
+
+
+;; =================
+;; Data definitions:
+
+(define-struct spec [label predicate])
+; Spec is a structure: (make-spec Label Predicate)
+
+(define school-schema.v2 `(,(make-spec "Name"    string?)
+                           ,(make-spec "Age"     integer?)
+                           ,(make-spec "Present" boolean?)))
+
+(define presence-schema.v2 `(,(make-spec "Present"     boolean?)
+                             ,(make-spec "Description" string?)))
