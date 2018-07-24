aad6f5a102070d593e537182e4a6f2e8a40356c6
Marcelo dos Santos <mdssjc@gmail.com>
2018-07-23 21:14:08 -0300

    Particle adicionado.

---
+++ clojure/the-nature-of-code/course/src/course/session2/particle.clj
@@ -0,0 +1,42 @@
+(ns course.session2.particle
+  (:import [processing.core PVector])
+  (:require [quil.core :as q]))
+
+(defn make-particle [m x y]
+  {:mass         m
+   :position     (new Particle x y)
+   :velocity     (new Particle 0 0)
+   :acceleration (new Particle 0 0)})
+
+(defn apply-force
+  "Newton's 2nd law: F = M * A or A = F / M"
+  [p force]
+  (let [f (PVector/div force (:mass p))]
+    (assoc p {:acceleration (PVector/add p f)})))
+
+(defn update [p]
+  (let [velocity (PVector/add p (:acceleration p))
+        position (PVector/add p velocity)]
+    (assoc p
+           {:position     position
+            :velocity     velocity
+            :acceleration 0})))
+
+(defn display [p]
+  (q/stroke 0)
+  (q/stroke-weight 2)
+  (q/fill 255 127)
+  (q/ellipse (get-in p [:position :x])
+             (get-in p [:position :y])
+             (* (:mass p) 16)
+             (* (:mass p) 16)))
+
+(defn check-edges [p]
+  (if (> (get-in p [:position :y]) (q/height))
+    (let [velocity-x (get-in p [:velocity :x])
+          velocity-y (* (get-in p [:velocity :y]) -0.9)
+          position-x (get-in p [:position :x])]
+      (assoc p
+             {:position (new PVector position-x (q/height))
+              :velocity (new PVector velocity-x velocity-y)}))
+    p))
