6b07dbe30e803a1d7bcea3e401d0576ec4718db7
Marcelo dos Santos <mdssjc@gmail.com>
2018-08-02 21:56:22 -0300

    Placeholder para Intermezzo 4: The Nature of Numbers.

---
+++ racket/HtDP2e/IV/Intermezzo 4.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |Intermezzo 4|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Intermezzo 4.rkt
+;; Intermezzo 4: The Nature of Numbers
