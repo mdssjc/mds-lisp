f09bbac48f991b3dd83910bb1ac347f6de9550e8
Marcelo dos Santos <mdssjc@gmail.com>
2018-08-06 21:53:24 -0300

    Continuação...

--- clojure/the-nature-of-code/book/src/book/chapter2/mover.clj
+++ clojure/the-nature-of-code/book/src/book/chapter2/mover.clj
@@ -2,15 +2,30 @@
   (:import [processing.core PVector])
   (:require [quil.core :as q]))
 
-(defn make-mover [x y]
-  {:location     (PVector. x y)
-   :velocity     (PVector. 0 0)
-   :acceleration (PVector. 0 0)})
+(defn make-mover
+  ([]
+   {:location     (PVector. (q/random (q/width))
+                            (q/random (q/height)))
+    :velocity     (PVector. 0 0)
+    :acceleration (PVector. 0 0)
+    :mass         10})
+  ([x y]
+   {:location     (PVector. x y)
+    :velocity     (PVector. 0 0)
+    :acceleration (PVector. 0 0)
+    :mass         1})
+  ([x y m]
+   {:location     (PVector. x y)
+    :velocity     (PVector. 0 0)
+    :acceleration (PVector. 0 0)
+    :mass         m}))
 
 (defn apply-force
   "Newton's 2nd law: F = M * A or A = F / M"
   [m force]
-  (assoc m :acceleration (.add (:acceleration m) force)))
+  (assoc m :acceleration (.add (:acceleration m)
+                               (.div (.get force)
+                                     (:mass m)))))
 
 (defn update [m]
   (let [acceleration (:acceleration m)
