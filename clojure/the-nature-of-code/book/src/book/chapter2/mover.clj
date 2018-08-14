3ea4e7faeed563a402e19a4c6822ab8159fe200f
Marcelo dos Santos <mdssjc@gmail.com>
2018-08-13 21:45:18 -0300

    Implementação do display e check-edges.

--- clojure/the-nature-of-code/book/src/book/chapter2/mover.clj
+++ clojure/the-nature-of-code/book/src/book/chapter2/mover.clj
@@ -4,11 +4,10 @@
 
 (defn make-mover
   ([]
-   {:location     (PVector. (q/random (q/width))
-                            (q/random (q/height)))
+   {:location     (PVector. 30 30)
     :velocity     (PVector. 0 0)
     :acceleration (PVector. 0 0)
-    :mass         10})
+    :mass         1})
   ([x y]
    {:location     (PVector. x y)
     :velocity     (PVector. 0 0)
@@ -37,7 +36,31 @@
            :acceleration (.mult acceleration 0))))
 
 (defn display [m]
-  (q/ellipse 0 0 0 0))
+  (q/stroke 0)
+  (q/fill 175)
+  (q/ellipse (.x (:location m))
+             (.y (:location m))
+             (*  (:mass m) 16)
+             (*  (:mass m) 16)))
+
+(defn check-edges [m]
+  (let [lx (.x (:location m))
+        ly (.y (:location m))
+        vx (.x (:velocity m))
+        vy (.y (:velocity m))
+        w  (q/width)
+        h  (q/height)]
+    (assoc m
+           :location (PVector. (cond (> lx w) w
+                                     (< lx 0) 0
+                                     :else    lx)
+                               (cond (> ly h) h
+                                     :else    ly))
+           :velocity (PVector. (cond (> lx w) (* vx -1)
+                                     (< lx 0) (* vx -1)
+                                     :else    vx)
+                               (cond (> ly h) (* vy -1)
+                                     :else    vy)))))
 
 
 ;; ---
