4c6a89e6589f350d574d1d52009881bbed2a6a03
Marcelo dos Santos <mdssjc@gmail.com>
2019-01-16 20:00:14 -0200

    BSL P5 - Compare Images.

---
+++ racket/HtC1x/1a/compare-images-starter.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname compare-images-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; compare-images-starter.rkt
+;; BSL P5 - Compare Images
+;; Write expressions to operate on booleans using primitives.
+
+(require 2htdp/image)
+
+
+;;
+;; PROBLEM:
+;;
+;; Based on the two constants provided, write three expressions to determine whether: 
+;;
+;; 1) IMAGE1 is taller than IMAGE2
+;; 2) IMAGE1 is narrower than IMAGE2
+;; 3) IMAGE1 has both the same width AND height as IMAGE2
+
+(define IMAGE1 (rectangle 10 15 "solid" "red"))
+(define IMAGE2 (rectangle 15 10 "solid" "red"))
