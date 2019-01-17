9f9d3da077a1f5b786f76c1d31db6f2d8713b78b
Marcelo dos Santos <mdssjc@gmail.com>
2019-01-17 19:48:57 -0200

    double-starter adicionado.

---
+++ racket/HtC1x/1b/double-starter.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname double-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; double-starter.rkt
+
+
+;;
+;; PROBLEM:
+;;
+;; Design a function that consumes a number and produces twice that number. 
+;; Call your function double. Follow the HtDF recipe and leave behind commented 
+;; out versions of the stub and template.
