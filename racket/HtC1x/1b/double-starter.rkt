5bdf33370fef9b5b4aa6c2c6abb1ff51719a6f35
Marcelo dos Santos <mdssjc@gmail.com>
2019-01-18 21:37:16 -0200

    double-starter implementado.

--- racket/HtC1x/1b/double-starter.rkt
+++ racket/HtC1x/1b/double-starter.rkt
@@ -10,3 +10,22 @@
 ;; Design a function that consumes a number and produces twice that number. 
 ;; Call your function double. Follow the HtDF recipe and leave behind commented 
 ;; out versions of the stub and template.
+
+;; Number -> Number (Signature)
+;; produce 2 times the given number (Purpose)
+
+;; (Stub)
+;(define (double n) 0)
+
+;; Examples/tests
+(check-expect (double 3) 6)
+(check-expect (double 4.2) (* 2 4.2))
+
+;; Template
+#;
+(define (double n)
+  (... n))
+
+;; Function Body
+(define (double n)
+  (* 2 n))
