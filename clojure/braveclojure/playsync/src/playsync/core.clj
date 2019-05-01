cce34bc6dba7d5e6c2e8dffe5e40e05e57e2f2c3
Marcelo dos Santos <mdssjc@gmail.com>
2019-04-30 22:39:55 -0300

    Processo de mensagem.

--- clojure/braveclojure/playsync/src/playsync/core.clj
+++ clojure/braveclojure/playsync/src/playsync/core.clj
@@ -1,7 +1,13 @@
 (ns playsync.core
-  (:gen-class))
+  (:require [clojure.core.async
+            :as a
+            :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def echo-chan (chan))
+(go (println (<! echo-chan)))
+(>!! echo-chan "ketchup")
