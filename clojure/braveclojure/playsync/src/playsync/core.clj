039b7f8736e93bb7e0b6e2120947b0f551f48f9d
Marcelo dos Santos <mdssjc@gmail.com>
2019-05-05 20:42:28 -0300

    API core.async.

--- clojure/braveclojure/playsync/src/playsync/core.clj
+++ clojure/braveclojure/playsync/src/playsync/core.clj
@@ -1,13 +1,54 @@
 (ns playsync.core
   (:require [clojure.core.async
-            :as a
-            :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]]))
+
+(defn append-to-file
+  "Write a string to the end of a file"
+  [filename s]
+  (spit filename s :append true))
+
+(defn format-quote
+  "Delineate the beginning and end of a quote because it's convenient"
+  [quote]
+  (str "=== BEGIN QUOTE ===\n" quote "=== END QUOTE ===\n\n"))
+
+(defn random-quote
+  "Retrieve a random quote and format it"
+  []
+  (format-quote (slurp "http://www.braveclojure.com/random-quote")))
+
+(defn snag-quotes
+  [filename num-quotes]
+  (let [c (chan)]
+    (go (while true (append-to-file filename (<! c))))
+    (dotimes [n num-quotes] (go (>! c (random-quote))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (snag-quotes "quotes" 2))
 
-(def echo-chan (chan))
-(go (println (<! echo-chan)))
-(>!! echo-chan "ketchup")
+(defn upper-caser
+  [in]
+  (let [out (chan)]
+    (go (while true (>! out (clojure.string/upper-case (<! in)))))
+    out))
+
+(defn reverser
+  [in]
+  (let [out (chan)]
+    (go (while true (>! out (clojure.string/reverse (<! in)))))
+    out))
+
+(defn printer
+  [in]
+  (go (while true (println (<! in)))))
+
+(def in-chan (chan))
+(def upper-caser-out (upper-caser in-chan))
+(def reverser-out (reverser upper-caser-out))
+(printer reverser-out)
+
+(>!! in-chan "redrum")
+(>!! in-chan "repaid")
