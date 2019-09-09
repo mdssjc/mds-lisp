5ac03e44bc00ac9c4f434d0c923a8e07de238e51
Marcelo dos Santos <mdssjc@gmail.com>
2019-09-08 21:25:24 -0300

    Modelando entidades com record.

--- clojure/clojure-applied/cljapplied/src/ch1/modeling.clj
+++ clojure/clojure-applied/cljapplied/src/ch1/modeling.clj
@@ -1,5 +1,6 @@
 (ns ch1.modeling)
 
+;; Map
 (def earth {:name       "Earth"
             :moons      1
             :volume     1.08321e12 ;; km^3
@@ -16,3 +17,23 @@
             :perihelion 147098290  ;; km, closest to sun
             :type       :Planet    ;; entity type
             })
+
+;; Record
+(defrecord Planet [name
+                   moons
+                   volume     ;; km^3
+                   mass       ;; kg
+                   aphelion   ;; km, farthest from sun
+                   perihelion ;; km, closest to sun
+                   ])
+
+(def earth
+     (->Planet "Earth" 1 1.08321e12 5.97219e24 152098232 147098290))
+
+(def earth
+     (map->Planet {:name       "Earth"
+                   :moons      1
+                   :volume     1.08321e12
+                   :mass       5.97219e24
+                   :aphelion   152098232
+                   :perihelion 147098290}))
