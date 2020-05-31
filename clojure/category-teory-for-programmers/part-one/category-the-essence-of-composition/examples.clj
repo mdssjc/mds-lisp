a5fdba1a50f0f80911b2c90f9ca56d0159ed5871
Marcelo dos Santos <mdssjc@gmail.com>
2020-05-30 21:36:35 -0300

    Category: The Essence of Composition.

--- clojure/category-teory-for-programmers/part-one/category-the-essence-of-composition/examples.clj
+++ clojure/category-teory-for-programmers/part-one/category-the-essence-of-composition/examples.clj
@@ -6,18 +6,27 @@
 
 (t/ann f [A -> B])
 (defn f [a]
-  nil)
+  a)
 
 (t/ann g [B -> C])
 (defn g [b]
-  nil)
+  b)
 
 (t/ann h [C -> D])
 (defn h [c]
-  nil)
+  c)
 
-;; Composition
+
+;; Composition Function
+
+(t/ann compose [f g -> x])
+(defn compose [f g]
+  (fn [x]
+    (f (g x))))
+
+(compose (compose f g) h)
 (comp h g f)
+
 
 ;; Identity Function
 
@@ -28,6 +37,25 @@
 (id 10)
 (identity 10)
 
+
+;; Tests
+
+(= (id ((compose inc inc) 2))
+   (id ((comp inc inc) 2)))
+
+((compose inc (partial * 2)) 2)
+((comp inc (partial * 2)) 2)
+
+
+
 ;; To summarize: A category consists of objects and arrows (morphisms).
 ;; Arrows can be composed, and the composition is associative.
 ;; Every object has an identity arrow that serves as a unit under composition.
+
+;; Challenges
+;; 1. Implement, as best as you can, the identity function in your favorite language (or the second favorite, if your favorite language) happens to be Haskell.
+;; 2. Implement the composition function in your favorite language. It takes two functions as arguments and returns a function that is their composition.
+;; 3. Write a program that tries to test that your composition function respects identity.
+;; 4. Is the world-wide web a category in any sense? Are links morphisms?
+;; 5. Is Facebook a category, with people as objects and friendships as morphisms?
+;; 6. When is a directed graph a category?
