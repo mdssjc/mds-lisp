be286c431fb532d36fff86bf1457be1ef0777894
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-03 20:14:27 -0300

    (WIP) Types and Functions.

---
+++ clojure/category-teory-for-programmers/part-one/types-and-functions/main.clj
@@ -0,0 +1,10 @@
+(ns main
+  (:require [clojure.core.typed :as t]))
+
+;;
+;; Types and Functions
+;;
+;; Who Needs Types?
+;; Types Are About Composability
+;; What Are Types?
+;; Why Do We Need a Mathematical Model?
