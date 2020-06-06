287b8fb19975a003f07a643a300ebde9cf86cc4c
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-05 22:50:28 -0300

    (WIP) Types and Functions.

--- clojure/category-teory-for-programmers/part-one/types-and-functions/main.clj
+++ clojure/category-teory-for-programmers/part-one/types-and-functions/main.clj
@@ -1,5 +1,6 @@
 (ns main
-  (:require [clojure.core.typed :as t]))
+  (:require [clojure.core.typed :refer :all]
+            [clojure.core.reducers :refer :all]))
 
 ;;
 ;; Types and Functions
@@ -8,3 +9,24 @@
 ;; Types Are About Composability
 ;; What Are Types?
 ;; Why Do We Need a Mathematical Model?
+;; Pure and Dirty Functions
+;; Examples of Types
+;;
+
+
+(defn fact [n]
+  (fold * (range 1 n)))
+
+(fact 6)
+
+(ann f44 [Nothing -> Number])
+(defn f44 []
+  44)
+
+(f44)
+
+(ann fInt [Number -> Nothing])
+(defn fInt [_]
+  '())
+
+(fInt 1)
