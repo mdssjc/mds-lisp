e5b7d0743d7460419f08ee64de490dd55bc30fc0
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-09 19:40:15 -0300

    Unifying Data with Sequences.

--- clojure/programming-clojure-3rd/ch03/sequences.clj
+++ clojure/programming-clojure-3rd/ch03/sequences.clj
@@ -67,3 +67,29 @@
 (set/difference languages beverages)
 (set/intersection languages beverages)
 (set/select #(= 1 (count %)) languages)
+
+
+(def compositions
+  #{{:name "The Art of the Fugue" :composer "J. S. Bach"}
+    {:name "Musical Offering" :composer "J. S. Bach"}
+    {:name "Requiem" :composer "Giuseppe Verdi"}
+    {:name "Requiem" :composer "W. A. Mozart"}})
+(def composers
+  #{{:composer "J. S. Bach" :country "Germany"}
+    {:composer "W. A. Mozart" :country "Austria"}
+    {:composer "Giuseppe Verdi" :country "Italy"}})
+(def nations
+  #{{:nation "Germany" :language "German"}
+    {:nation "Austria" :language "German"}
+    {:nation "Italy" :language "Italian"}})
+
+(set/rename compositions {:name :title})
+(set/select #(=(:name %) "Requiem") compositions)
+(set/project compositions [:name])
+(set/join compositions composers)
+(set/join composers nations {:country :nation})
+(set/project
+ (set/join
+  (set/select #(=(:name %) "Requiem") compositions)
+  composers)
+ [:country])
