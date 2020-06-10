a72d2b764ca71d7130fe78b53072375987ead631
Marcelo dos Santos <mdssjc@gmail.com>
2020-06-09 21:45:36 -0300

    (WIP) Functional Programming.

---
+++ clojure/programming-clojure-3rd/ch04/functional.clj
@@ -0,0 +1,11 @@
+(ns functional)
+
+(defn stack-consuming-fibo [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+ (stack-consuming-fibo (- n 1))
+             (stack-consuming-fibo (- n 2)))))
+
+(stack-consuming-fibo 9)
+(stack-consuming-fibo 1000000)
