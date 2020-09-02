3a374072c34a9878647ec30c1e6106d92d8ad976
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-01 22:20:42 -0300

    (WIP) Specifications.

--- clojure/programming-clojure-3rd/ch05/spec.clj
+++ clojure/programming-clojure-3rd/ch05/spec.clj
@@ -39,3 +39,49 @@
           :opt-un [::music/artist
                    ::music/title
                    ::music/date]))
+
+
+
+(s/def ::rand-args (s/cat :n (s/? number?)))
+(s/def ::rand-ret double?)
+
+(s/def ::rand-fn
+  (fn [{:keys [args ret]}]
+    (let [n (or (:n args) 1)]
+      (cond (zero? n) (zero? ret)
+            (pos? n) (and (>= ret 0) (< ret n))
+            (neg? n) (and (<= ret 0) (> ret n))))))
+
+(s/fdef clojure.core/rand
+  :args ::rand-args
+  :ret ::rand-ret
+  :fn ::rand-fn)
+
+(stest/instrument 'clojure.core/rand)
+(stest/instrument (stest/enumerate-namespace 'clojure.core))
+
+(rand :boom)
+
+
+
+(defn opposite [pred]
+  (comp not pred))
+
+(s/def ::pred
+  (s/fspec :args (s/cat :x any?)
+           :ret boolean?))
+
+(s/fdef opposite
+  :args (s/cat :pred ::pred)
+  :ret ::pred)
+
+
+
+(s/fdef clojure.core/symbol
+  :args (s/cat :ns (s/? string?) :name string?)
+  :ret symbol?
+  :fn (fn [{:keys [args ret]}]
+        (and (= (name ret) (:name args))
+             (= (namespace ret) (:ns args)))))
+
+(stest/check 'clojure.core/symbol)
