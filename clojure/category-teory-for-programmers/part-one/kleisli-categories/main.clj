7b6a16f7196517fce33f43ceb8e8f0c69ee91c07
Marcelo dos Santos <mdssjc@gmail.com>
2020-09-05 18:35:59 -0300

    (WIP) Kleisli Categories.

---
+++ clojure/category-teory-for-programmers/part-one/kleisli-categories/main.clj
@@ -0,0 +1,18 @@
+(ns main)
+
+;;
+;; Kleisli Categories
+;;
+;; The Writer Category
+;; Writer in Haskell
+;; Kleisli Categories
+;; Challenges
+;;
+
+
+
+;; Challenges
+
+;; 1. Construct the Kleisli category for partial functions (define composition and identity).
+;; 2. Implement the embellished function safe_reciprocal that returns a valid reciprocal of its argument, if it’s different from zero.
+;; 3. Compose the functions safe_root and safe_reciprocal to implement safe_root_reciprocal that calculates sqrt(1/x) whenever possible.
